e1eef4e6b8c2e3b9c2a7c763050a3776b70d95ba
Nick Kosarev <hmbanan666@hotmail.com>
2023-12-07 13:20:55 +0200

    Optimize handling of DNS queries and environment variables

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,6 +1,13 @@
 import { Answer, CaaAnswer, CaaData, Packet, Question } from 'dns-packet';
+import * as dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 const BASE_TTL = process.env.BASE_TTL ? Number(process.env.BASE_TTL) : 3600;
+const DOMAIN_SERVER_IP = process.env.DOMAIN_SERVER_IP;
+const CAA_RECORD_1 = process.env.CAA_RECORD_1;
+const NAME_SERVERS: string[] = getNameServersFromEnvironmentVariables();
 
 export const handleDnsQuery = (req: Packet): Packet | null => {
   const answers: Answer[] | null = getAnswers(req);
@@ -33,21 +40,17 @@ const handleQuestion = (question: Question): Answer[] => {
   }
 };
 
-const handleATypeQuestion = (name: string): Answer[] => {
-  const domainServerIp = process.env.DOMAIN_SERVER_IP;
-  return domainServerIp ? [prepareAnswerForA(name, domainServerIp)] : [];
-};
+const handleATypeQuestion = (name: string): Answer[] =>
+  DOMAIN_SERVER_IP ? [prepareAnswerForA(name, DOMAIN_SERVER_IP)] : [];
 
-const handleNSTypeQuestion = (name: string): Answer[] => {
-  return prepareAnswersForNS(name);
-};
+const handleNSTypeQuestion = (name: string): Answer[] =>
+  NAME_SERVERS.map((ns) => prepareAnswer(name, ns, 'NS'));
 
 const handleCAATypeQuestion = (name: string): Answer[] => {
-  const record = process.env.CAA_RECORD_1;
-  if (record) {
+  if (CAA_RECORD_1) {
     const caaData: CaaData = {
       tag: 'issue',
-      value: record,
+      value: CAA_RECORD_1,
     };
     return [prepareAnswerForCAA(name, caaData)];
   }
@@ -55,20 +58,8 @@ const handleCAATypeQuestion = (name: string): Answer[] => {
   return [];
 };
 
-const prepareAnswerForA = (name: string, domainServerIp: string) => {
-  return prepareAnswer(name, domainServerIp, 'A');
-};
-
-const prepareAnswersForNS = (name: string): Answer[] => {
-  const answers: Answer[] = [];
-  for (let i = 1; i <= 4; i += 1) {
-    const nameServerEnv = process.env[`NAME_SERVER_${i}`];
-    if (nameServerEnv) {
-      answers.push(prepareAnswer(name, nameServerEnv, 'NS', BASE_TTL));
-    }
-  }
-  return answers;
-};
+const prepareAnswerForA = (name: string, domainServerIp: string) =>
+  prepareAnswer(name, domainServerIp, 'A');
 
 const formResponsePacket = (req: Packet, answers: Answer[]): Packet => {
   return {
@@ -83,13 +74,12 @@ const prepareAnswer = (
   name: string,
   data: string,
   type: 'A' | 'NS',
-  ttl = BASE_TTL,
 ): Answer => {
   return {
     name,
     data,
     type,
-    ttl,
+    ttl: BASE_TTL,
     class: 'IN',
     flush: false,
   };
@@ -105,3 +95,20 @@ const prepareAnswerForCAA = (name: string, data: CaaData): CaaAnswer => {
     flush: false,
   };
 };
+
+function getNameServersFromEnvironmentVariables(): string[] {
+  const NUMBER_OF_NAME_SERVERS = 4;
+  const NAME_SERVER_ERROR_MESSAGE =
+    'Required environment variables NAME_SERVER_1, NAME_SERVER_2, NAME_SERVER_3, NAME_SERVER_4 are not set';
+
+  const nameServers = Array.from(
+    { length: NUMBER_OF_NAME_SERVERS },
+    (_, i) => process.env[`NAME_SERVER_${i + 1}`] || '',
+  ).filter(Boolean);
+
+  if (!nameServers.length) {
+    throw new Error(NAME_SERVER_ERROR_MESSAGE);
+  }
+
+  return nameServers;
+}
